01b1aaab837c9e049942101eb4da6660610afbf9
Shivani Joshi <46731446+Shivani912@users.noreply.github.com>
2019-08-06 19:33:43 -0500

    added negative test case

--- testing/main_test.go
+++ testing/main_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 )
 
+// TestCase1 and TestCase2 are positive test cases
+
 func TestCase1(t *testing.T) {
 	folder := "./testCase1/"
 	input, expectedOutput := getData(folder)
@@ -20,6 +22,20 @@ func TestCase2(t *testing.T) {
 	input, expectedOutput := getData(folder)
 
 	if output := string(toHex(input.Hash())); !(output == expectedOutput) {
+		t.Errorf("Test failed: %v inputted, %s expected, received: %s", input, expectedOutput, output)
+
+	}
+
+}
+
+// TestCase3 is a negative test case
+// the merkle root in file is incorrect
+// therefore output should never be equal to expectedOutput
+func TestCase3(t *testing.T) {
+	folder := "./testCase3/"
+	input, expectedOutput := getData(folder)
+
+	if output := string(toHex(input.Hash())); output == expectedOutput {
 		t.Errorf("Test failed: %v inputted, %s expected, received: %s", input, expectedOutput, output)
 
 	}
