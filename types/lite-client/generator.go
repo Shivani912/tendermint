33fb2670aba7a3bae2d0860c3761acc78d9748e0
Shivani Joshi <joshi.shivani912@gmail.com>
2019-11-29 15:59:03 -0500

    changed example function

--- types/lite-client/generator.go
+++ types/lite-client/generator.go
@@ -4,6 +4,5 @@ import "github.com/tendermint/tendermint/types/lite-client/generator"
 
 func main() {
 
-	// generator.GenerateValList(128, 50)
 	generator.GenerateTestCases("./tests/json/val_list.json", "./tests/json/test_lite_client.json")
 }
