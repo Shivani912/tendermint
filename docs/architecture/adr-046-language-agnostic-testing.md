9cbab22c1e1eeb631a1d2e2fce85587c08d86f67
Shivani Joshi <joshi.shivani912@gmail.com>
2020-01-10 16:27:50 -0500

    minor structural change in testcases

--- docs/architecture/adr-046-language-agnostic-testing.md
+++ docs/architecture/adr-046-language-agnostic-testing.md
@@ -6,24 +6,47 @@
 
 ## Context
 
-Currently, we are far from reaching Tendermint v1.0 and expecting quite a few breaking changes on our way. We are positively progressing towards a better version and this means, specs and infrastructure are evolving by every release. While this is the status of Tendermint in Go, we have also begun to work on Tendermint in Rust in parallel (also have javascript implementation). Moving forward with Rust implementation at this stage can be difficult to keep up with the changes being introduced at every stage in Go implementation.
+... TODO: include some background on light client, we're starting there. 
+what does a light client need to function? nodes serving headers and commits, and validator sets ...
+sequential and bisection. fact of multiple peers.
 
-To argue this, we do have an English spec that explains the behaviour and implementation of components. However, it clearly does not cover the run time behaviour of data sets. We do follow a test driven development, but again, that does not test integration of multiple components within Tendermint. Also, considering how different coding-style in Rust can be, the implementation will totally differ than in Go. This makes it very easy to develop significant gap between these two implementations.
 
-So to facilitate implementation agnosticism while replicating behaviour of Tendermint, language agnostic tests seem to be the bridge between the gap. It lets you really be precise about how the data should be manipulated and decisions to be made within the core logic. You can simulate situations that needs to be tested and define what the expected behaviour should be. There can be multiple levels of testing ranging from unit testing to system-wide testing.
+Currently, we are far from reaching Tendermint v1.0 and expecting quite a few breaking changes on our way. We are positively progressing towards a better version and this means, specs and infrastructure are evolving by every release. While this is the status of Tendermint in Go, we have also begun to work on Tendermint in Rust in parallel, while others are working on implementations in other languages. Moving forward with Rust implementation at this stage can be difficult to keep up with the changes being introduced at every stage in Go implementation.
 
+while we do have eng spec we cant test directly. when we have 2 diff impl we need a way to see it...
+
+To argue this, we do have an English spec that explains the behaviour and implementation of components. However, it clearly does not cover the run time behaviour of data sets. We do follow a test driven development, but again, that does not test integration of multiple components within Tendermint. Also, considering how different coding-style(language specific restrictions and perks, features so we want a common thing that can be used to test that they are compatible) in Rust can be, the implementation will totally differ than in Go. This makes it very easy to develop significant gap between these two implementations.
+
+So to facilitate implementation agnosticism while replicating behaviour of Tendermint, language agnostic tests seem to be the bridge between the gap. It lets you really be precise about how the data should be manipulated and formatted, and decisions to be made within the core logic. You can simulate situations that needs to be tested and define what the expected behaviour should be. There can be multiple levels of testing ranging from unit testing to system-wide testing.
+
+(go impl as reference - we use it to generate but need to be careful of defining the output)
 Considering the behaviour outlined by Go implementation to be the standard, we make assertions on that basis but it is not limited to it. Therefore, we also have a good scope on finding existing vulnerabilities(if any) in the system. For producing concrete test cases, we can highly rely on TLC output from Lite Client TLA+ specification. TLC output can be parsed to the JSON test case format, mocking out the remaining data, which makes it acceptable by the main code and can be tested against tests in each implementation.
+
+- we should have files with all the data to test against, eg. using json
+- great example is the ethereum test suite ...
+- we want to limit the manual work to generate test cases, so ideally we'd be able to use outputs from TLC 
 
 ## Decision
 
+TODO: record that we're going to use JSON and why
+
 - At a high level, this can be separated into two different tasks: 
 	1. generating test files, and 
-	2. writing tests
+	2. testing the test files
 
-* Generating test files:
+### Generating test files
+
+TODO: explain what needs to be generated ... we need to make a chain of blocks
+many functions already exist to do this as they are used in the Go unit tests.
+We want to translate that into JSON blockchains ..
+Note something about the difference between these functions and the real functions for making blocks ...
+I think it mostly boils down to private keys ...
 
 	- Generators use low-level test functions, manually configured in a way that outputs test cases, each representing a unique possible situation within Tendermint.
 	- In case of lite client, a test case could look like this:
+
+A light client test case begins with an initial state (a trusted header and validator set)
+and takes a series of inputs in the form of what we call LiteBlock, which contains a header, commit, and validator set:
 ```
 type TestCase struct {
     TestName       string      `json:"test_name"`
@@ -49,7 +72,7 @@ type Initial struct {
 
 - TestName is a one word identifier for the test case that tells what functionality of the code we are testing.
 - Description is a brief explanation of what the test case is i.e. what situation the case represents.
-- Initial is the starting state or to say, a context for the test case. In terms of Lite Client, it can be the trusted state. In case of testing bisection, initial will have primary and alternative providers, trusted store, trust options, etc. which make the Client.
+- Initial is the starting state or to say, a context for the test case. In terms of Lite Client, it can be the trusted state. In case of testing bisection, initial will have primary and alternative providers, trusted store, trust options, etc. which make the Client. ... TODO: explain this better / simpler
 - Input is the actual data that is to be tested. In the case where we have multiple providers to fetch data from, input can be an array of array of LiteBlock i.e [][]LiteBlock, where [0][0]LiteBlock refers to the first LiteBlock of first provider.
 - Expected output, for now, is a string that tells whether to expect an error or not. This will change once we have standardized error handling and should be error types or represent certain state.
 
--- types/lite-client/generator/verify_val_set.go
+++ types/lite-client/generator/verify_val_set.go
@@ -4,138 +4,138 @@ import (
 	"github.com/tendermint/tendermint/types"
 )
 
-func CaseVerifyValidatorSetOf1(testCases *TestCases, valList ValList) {
+func caseVerifyValidatorSetOf1(testBatch *TestBatch, valList ValList) {
 
 	description := "Case: one lite block to fetch, one validator in the set, expects no error"
 
-	initial, input, _, _ := GenerateGeneralCase(valList, 1)
-	testCase := GenerateTestCase(testName, description, initial, input, expectedOutputNoError)
+	initial, input, _, _ := generateGeneralCase(valList, 1)
+	testCase := generateTestCase(description, initial, input, expectedOutputNoError)
 
-	testCases.TC = append(testCases.TC, testCase)
+	testBatch.TestCases = append(testBatch.TestCases, testCase)
 }
 
-func CaseVerifyValidatorSetOf8(testCases *TestCases, valList ValList) {
+func caseVerifyValidatorSetOf8(testBatch *TestBatch, valList ValList) {
 
 	description := "Case: one lite block to fetch, 8 validators in the set, expects no error"
-	initial, input, _, _ := GenerateGeneralCase(valList, 8)
-	testCase := GenerateTestCase(testName, description, initial, input, expectedOutputNoError)
-	testCases.TC = append(testCases.TC, testCase)
+	initial, input, _, _ := generateGeneralCase(valList, 8)
+	testCase := generateTestCase(description, initial, input, expectedOutputNoError)
+	testBatch.TestCases = append(testBatch.TestCases, testCase)
 }
 
-func CaseVerifyValidatorSetOf128(testCases *TestCases, valList ValList) {
+func caseVerifyValidatorSetOf128(testBatch *TestBatch, valList ValList) {
 
 	description := "Case: one lite block, 128 validators, no error"
-	initial, input, _, _ := GenerateGeneralCase(valList, 128)
-	testCase := GenerateTestCase(testName, description, initial, input, expectedOutputNoError)
-	testCases.TC = append(testCases.TC, testCase)
+	initial, input, _, _ := generateGeneralCase(valList, 128)
+	testCase := generateTestCase(description, initial, input, expectedOutputNoError)
+	testBatch.TestCases = append(testBatch.TestCases, testCase)
 
 }
 
-func CaseVerifyValidatorSetEmpty(testCases *TestCases, valList ValList) {
+func caseVerifyValidatorSetEmpty(testBatch *TestBatch, valList ValList) {
 
 	description := "Case: one lite block, empty validator set, expects error"
-	initial, input, _, _ := GenerateGeneralCase(valList, 2)
+	initial, input, _, _ := generateGeneralCase(valList, 2)
 	input[0].ValidatorSet = *types.NewValidatorSet(nil)
-	testCase := GenerateTestCase(testName, description, initial, input, expectedOutputError)
+	testCase := generateTestCase(description, initial, input, expectedOutputError)
 
-	testCases.TC = append(testCases.TC, testCase)
+	testBatch.TestCases = append(testBatch.TestCases, testCase)
 
 }
 
-func CaseVerifyValidatorSetAddTwiceVals(testCases *TestCases, valList ValList) {
+func caseVerifyValidatorSetAddTwiceVals(testBatch *TestBatch, valList ValList) {
 
 	description := "Case: two lite blocks, validator set reduces to half, no error"
 
-	initial, input, _, _ := GenerateNextValsUpdateCase(valList, 2, 2, 0)
-	testCase := GenerateTestCase(testName, description, initial, input, expectedOutputNoError)
-	testCases.TC = append(testCases.TC, testCase)
+	initial, input, _, _ := generateNextValsUpdateCase(valList, 2, 2, 0)
+	testCase := generateTestCase(description, initial, input, expectedOutputNoError)
+	testBatch.TestCases = append(testBatch.TestCases, testCase)
 }
 
-func CaseVerifyValidatorSetRemoveHalfVals(testCases *TestCases, valList ValList) {
+func caseVerifyValidatorSetRemoveHalfVals(testBatch *TestBatch, valList ValList) {
 
 	copyValList := valList.Copy()
 	description := "Case: two lite blocks, validator set reduces to half, no error"
 
-	initial, input, _, _ := GenerateNextValsUpdateCase(copyValList, 4, 0, 2)
+	initial, input, _, _ := generateNextValsUpdateCase(copyValList, 4, 0, 2)
 
-	testCase := GenerateTestCase(testName, description, initial, input, expectedOutputNoError)
-	testCases.TC = append(testCases.TC, testCase)
+	testCase := generateTestCase(description, initial, input, expectedOutputNoError)
+	testBatch.TestCases = append(testBatch.TestCases, testCase)
 
 }
 
-func CaseVerifyValidatorSetChangesOneThird(testCases *TestCases, valList ValList) {
+func caseVerifyValidatorSetChangesOneThird(testBatch *TestBatch, valList ValList) {
 
 	copyValList := valList.Copy()
 	description := "Case: two lite blocks, 1/3 validator set changes, no error"
 
-	initial, input, _, _ := GenerateNextValsUpdateCase(copyValList, 3, 1, 1)
-	testCase := GenerateTestCase(testName, description, initial, input, expectedOutputNoError)
+	initial, input, _, _ := generateNextValsUpdateCase(copyValList, 3, 1, 1)
+	testCase := generateTestCase(description, initial, input, expectedOutputNoError)
 
-	testCases.TC = append(testCases.TC, testCase)
+	testBatch.TestCases = append(testBatch.TestCases, testCase)
 }
 
-func CaseVerifyValidatorSetChangesHalf(testCases *TestCases, valList ValList) {
+func caseVerifyValidatorSetChangesHalf(testBatch *TestBatch, valList ValList) {
 
 	copyValList := valList.Copy()
 	description := "Case: two lite blocks, 1/2 validator set changes, no error"
 
-	initial, input, _, _ := GenerateNextValsUpdateCase(copyValList, 4, 2, 2)
-	testCase := GenerateTestCase(testName, description, initial, input, expectedOutputNoError)
+	initial, input, _, _ := generateNextValsUpdateCase(copyValList, 4, 2, 2)
+	testCase := generateTestCase(description, initial, input, expectedOutputNoError)
 
-	testCases.TC = append(testCases.TC, testCase)
+	testBatch.TestCases = append(testBatch.TestCases, testCase)
 }
 
-func CaseVerifyValidatorSetChangesTwoThirds(testCases *TestCases, valList ValList) {
+func caseVerifyValidatorSetChangesTwoThirds(testBatch *TestBatch, valList ValList) {
 
 	copyValList := valList.Copy()
 	description := "Case: two lite blocks, 2/3 validator set changes, no error"
 
-	initial, input, _, _ := GenerateNextValsUpdateCase(copyValList, 3, 2, 2)
-	testCase := GenerateTestCase(testName, description, initial, input, expectedOutputNoError)
+	initial, input, _, _ := generateNextValsUpdateCase(copyValList, 3, 2, 2)
+	testCase := generateTestCase(description, initial, input, expectedOutputNoError)
 
-	testCases.TC = append(testCases.TC, testCase)
+	testBatch.TestCases = append(testBatch.TestCases, testCase)
 }
 
-func CaseVerifyValidatorSetChangesFully(testCases *TestCases, valList ValList) {
+func caseVerifyValidatorSetChangesFully(testBatch *TestBatch, valList ValList) {
 
 	copyValList := valList.Copy()
 	description := "Case: two lite blocks, validator set changes completely, no error"
 
-	initial, input, _, _ := GenerateNextValsUpdateCase(copyValList, 5, 5, 5)
-	testCase := GenerateTestCase(testName, description, initial, input, expectedOutputNoError)
+	initial, input, _, _ := generateNextValsUpdateCase(copyValList, 5, 5, 5)
+	testCase := generateTestCase(description, initial, input, expectedOutputNoError)
 
-	testCases.TC = append(testCases.TC, testCase)
+	testBatch.TestCases = append(testBatch.TestCases, testCase)
 }
 
-func CaseVerifyValidatorSetChangesLessThanOneThird(testCases *TestCases, valList ValList) {
+func caseVerifyValidatorSetChangesLessThanOneThird(testBatch *TestBatch, valList ValList) {
 
 	copyValList := valList.Copy()
 	description := "Case: two lite blocks, less than 1/3 validator set changes, no error"
 
-	initial, input, _, _ := GenerateNextValsUpdateCase(copyValList, 4, 1, 1)
-	testCase := GenerateTestCase(testName, description, initial, input, expectedOutputNoError)
+	initial, input, _, _ := generateNextValsUpdateCase(copyValList, 4, 1, 1)
+	testCase := generateTestCase(description, initial, input, expectedOutputNoError)
 
-	testCases.TC = append(testCases.TC, testCase)
+	testBatch.TestCases = append(testBatch.TestCases, testCase)
 }
 
-func CaseVerifyValidatorSetChangesMoreThanTwoThirds(testCases *TestCases, valList ValList) {
+func caseVerifyValidatorSetChangesMoreThanTwoThirds(testBatch *TestBatch, valList ValList) {
 
 	copyValList := valList.Copy()
 	description := "Case: two lite blocks, more than 2/3 validator set changes, no error"
 
-	initial, input, _, _ := GenerateNextValsUpdateCase(copyValList, 4, 3, 3)
-	testCase := GenerateTestCase(testName, description, initial, input, expectedOutputNoError)
+	initial, input, _, _ := generateNextValsUpdateCase(copyValList, 4, 3, 3)
+	testCase := generateTestCase(description, initial, input, expectedOutputNoError)
 
-	testCases.TC = append(testCases.TC, testCase)
+	testBatch.TestCases = append(testBatch.TestCases, testCase)
 }
 
-func CaseVerifyValidatorSetWrongValidatorSet(testCases *TestCases, valList ValList) {
+func caseVerifyValidatorSetWrongValidatorSet(testBatch *TestBatch, valList ValList) {
 
 	var input []LiteBlock
 	description := "Case: one lite block, wrong validator set, expects error"
 
-	signedHeader, state, _ := GenerateFirstBlock(valList, 3, firstBlockTime)
-	initial := GenerateInitial(signedHeader, *state.NextValidators, trustingPeriod, now)
+	signedHeader, state, _ := generateFirstBlock(valList, 3, firstBlockTime)
+	initial := generateInitial(signedHeader, *state.NextValidators, trustingPeriod, now)
 
 	wrongVals := valList.Validators[3:6]
 	wrongPrivVals := valList.PrivVals[3:6]
@@ -143,48 +143,48 @@ func CaseVerifyValidatorSetWrongValidatorSet(testCases *TestCases, valList ValLi
 	state.Validators = wrongValSet
 	state.NextValidators = wrongValSet
 
-	liteBlock, state := GenerateNextBlock(state, wrongPrivVals, initial.SignedHeader.Commit, secondBlockTime)
+	liteBlock, state := generateNextBlock(state, wrongPrivVals, initial.SignedHeader.Commit, secondBlockTime)
 	input = append(input, liteBlock)
-	testCase := GenerateTestCase(testName, description, initial, input, expectedOutputError)
+	testCase := generateTestCase(description, initial, input, expectedOutputError)
 
-	testCases.TC = append(testCases.TC, testCase)
+	testBatch.TestCases = append(testBatch.TestCases, testCase)
 }
 
-func CaseVerifyValidatorSetReplaceValidator(testCases *TestCases, valList ValList) {
+func caseVerifyValidatorSetReplaceValidator(testBatch *TestBatch, valList ValList) {
 
 	copyValList := valList.Copy()
 	var input []LiteBlock
 	description := "Case: one lite block, replacing a validator in validator set, expects error"
 
-	signedHeader, state, privVals := GenerateFirstBlock(copyValList, 3, firstBlockTime)
-	initial := GenerateInitial(signedHeader, *state.NextValidators, trustingPeriod, now)
+	signedHeader, state, privVals := generateFirstBlock(copyValList, 3, firstBlockTime)
+	initial := generateInitial(signedHeader, *state.NextValidators, trustingPeriod, now)
 
 	privVals[0] = copyValList.PrivVals[4]
 	state.Validators.Validators[0] = copyValList.Validators[4]
 	state.NextValidators = state.Validators
 
-	liteBlock, state := GenerateNextBlock(state, privVals, initial.SignedHeader.Commit, secondBlockTime)
+	liteBlock, state := generateNextBlock(state, privVals, initial.SignedHeader.Commit, secondBlockTime)
 	input = append(input, liteBlock)
-	testCase := GenerateTestCase(testName, description, initial, input, expectedOutputError)
+	testCase := generateTestCase(description, initial, input, expectedOutputError)
 
-	testCases.TC = append(testCases.TC, testCase)
+	testBatch.TestCases = append(testBatch.TestCases, testCase)
 }
 
-func CaseVerifyValidatorSetChangeValidatorPower(testCases *TestCases, valList ValList) {
+func caseVerifyValidatorSetChangeValidatorPower(testBatch *TestBatch, valList ValList) {
 
 	copyValList := valList.Copy()
 	var input []LiteBlock
 	description := "Case: one lite block, changing a validator's power in validator set, expects error"
 
-	signedHeader, state, privVals := GenerateFirstBlock(copyValList, 3, firstBlockTime)
-	initial := GenerateInitial(signedHeader, *state.NextValidators, trustingPeriod, now)
+	signedHeader, state, privVals := generateFirstBlock(copyValList, 3, firstBlockTime)
+	initial := generateInitial(signedHeader, *state.NextValidators, trustingPeriod, now)
 
-	state.Validators.Validators[0].VotingPower += 1
+	state.Validators.Validators[0].VotingPower++
 	state.NextValidators = state.Validators
 
-	liteBlock, state := GenerateNextBlock(state, privVals, initial.SignedHeader.Commit, secondBlockTime)
+	liteBlock, state := generateNextBlock(state, privVals, initial.SignedHeader.Commit, secondBlockTime)
 	input = append(input, liteBlock)
-	testCase := GenerateTestCase(testName, description, initial, input, expectedOutputError)
+	testCase := generateTestCase(description, initial, input, expectedOutputError)
 
-	testCases.TC = append(testCases.TC, testCase)
+	testBatch.TestCases = append(testBatch.TestCases, testCase)
 }
